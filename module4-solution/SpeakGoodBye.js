5ecac6042c2e75dcb9e2d7fc7cdd0e0b342f2b1f
Yunxiang Chang <yunxiangchang@gmail.com>
2020-07-12 16:09:49 -0400

    m4 harder solution

---
+++ module4-solution/SpeakGoodBye.js
@@ -0,0 +1,11 @@
+(function () {
+	var speakWord = "Good Bye";
+
+	var byeSpeaker = {
+		speak: function (name) {
+			console.log(speakWord + " " + name);
+		}
+	}
+
+	window.byeSpeaker = byeSpeaker;
+})();
---
+++ module4-solution/SpeakHello.js
@@ -0,0 +1,11 @@
+(function () {
+	var speakWord = "Hello";
+
+	var helloSpeaker = {
+		speak: function (name) {
+			console.log(speakWord + " " + name);
+		}
+	} 
+
+	window.helloSpeaker = helloSpeaker;
+})();
